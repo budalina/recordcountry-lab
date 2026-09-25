046cb97f26170321350863297850b7f7f7dfe339
Budaeva Alina <budalina@yandex.ru>
2026-09-25 14:01:23 +0300

    Добавлен статический блок для первоначальной инициализации массива стран

--- src/recordcountry/CountryService.java
+++ src/recordcountry/CountryService.java
@@ -2,4 +2,14 @@ package recordcountry;
 
 public class CountryService {
     private static Strana[] country ;
+
+    static {
+        country= new Strana[] {
+                new Strana("Russia", "Moscow", 17.1, 148),
+                new Strana("France", "Paris", 27.1, 137),
+                new Strana("Great Britain", "London", 13.1, 150),
+                new Strana("Italy", "Rome", 18.1, 104)
+        };
+    }
+
 }
