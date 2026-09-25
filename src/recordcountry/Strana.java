bc326551d522e369fed96f8bd89d26bf9624d7e3
Budaeva Alina <budalina@yandex.ru>
2026-09-25 15:39:06 +0300

    Исправлено значение площади для России

--- src/recordcountry/Strana.java
+++ src/recordcountry/Strana.java
@@ -12,7 +12,7 @@ public class Strana {
 
     static {
         country= new Strana[] {
-                new Strana("Russia", "Moscow", 17.1, 148),
+                new Strana("Russia", "Moscow", 107.1, 148),
                 new Strana("France", "Paris", 27.1, 137),
                 new Strana("Great Britain", "London", 13.1, 150),
                 new Strana("Italy", "Rome", 18.1, 104)
